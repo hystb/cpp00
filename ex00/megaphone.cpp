dbb84e14b42cc291ce841e63e1cffb81c2e487f5
Nicolas Milan <nmilan@z4r7p7.42lyon.fr>
2023-06-23 10:50:38 +0200

    fix trim

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+void	ft_trim(std::string str, char *res)
+{
+	size_t	i = 0;
+	size_t	j = 0;
+
+	i = str.find_first_not_of(" ");
+	if (i == std::string::npos)
+	{
+		res[0] = '\0';
+		return ;
+	}
+	j = str.find_last_not_of(" ");
+	str.copy(res, j - i + 1, i);
+	res[j - i + 1] = '\0';
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
-	int		i = 0;
 	char	*str = NULL;
 	char	c = 0;
 
@@ -13,6 +29,7 @@ int	main(int argc, char **argv)
 		argv++;
 		while(*argv)
 		{
+			ft_trim(*argv, *argv);
 			str = *argv;
 			while (*str)
 			{
